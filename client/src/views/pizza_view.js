5482bce6eea14bc4ec76f0f7a10e576fa3f485eb
Kris Brough <a0109728@0109728s-MacBook-Pro.local>
2019-04-25 13:18:09 +0100

    PizzaView diplayPizza()

--- client/src/views/pizza_view.js
+++ client/src/views/pizza_view.js
@@ -1,10 +1,38 @@
 class PizzaView{
 
   constructor(){
-    this.container = document.querySelector()
+    this.container = document.querySelector('#display-pizza')
+  }
+
+  bindEvents(){
+    PubSub.subscribe("pizza:pizza-selection-ready", (event) => {
+      const pizza = event.detail;
+      this.displayPizza(pizza);
+    })
+  }
+
+  displayPizza(pizza){
+    const image = document.createElement('img');
+    image.src = pizza.image;
+    image.classList.add("");
+
+    const description = document.createElement('p');
+    description.textContent = pizza.description;
+    description.classList.add("");
+
+    const toppingsUL = document.createElement('ul');
+    const toppings = pizza.toppings;
+    toppings.forEach((topping) => {
+      const toppingLI = document.createElement('li');
+      toppingLI.textContent = topping;
+      toppingsUL.appendChild(topping);
+    });
+
+    this.container.appendChild(image);
+    this.container.appendChild(description);
+    this.container.appendChild(toppingsUL);
 
   }
 
-  
 }
 module.exports = PizzaView;
